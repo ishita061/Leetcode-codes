2b4368462210be9181c9e96ae18c3a69b62e60e7
Ishita chaturvedi <168914437+ishita061@users.noreply.github.com>
2025-10-09 14:44:40 +0530

    Update and rename Pascals-Triangle to Pascals-Triangle.java

--- Dynamic-Programming/Pascals-Triangle.java
+++ Dynamic-Programming/Pascals-Triangle.java
@@ -1,10 +1,10 @@
-118. Pascal's Triangle
+//118. Pascal's Triangle
 
 
-Given an integer numRows, return the first numRows of Pascal's triangle.
-Example 1:
-Input: numRows = 5
-Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+//Given an integer numRows, return the first numRows of Pascal's triangle.
+//Example 1:
+//Input: numRows = 5
+//Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
 
 class Solution {
